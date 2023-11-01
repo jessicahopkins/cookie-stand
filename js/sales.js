0ed6e13f35d5ddd93c4c23d15902c0741d95c3a0
Jessica Hopkins <jhopkins@mutualofenumclaw.com>
2023-11-01 15:35:00 -0700

    modified sales.js and sales.html to get the table formated properly and code working

--- js/sales.js
+++ js/sales.js
@@ -1,46 +1,100 @@
+"use strict";
+// Thanks to the code review from Brendan on 11/1 in class, I was able to model after his code to get this working.
+let stores = [];
+let totals = new Array(14);
+totals.fill(0);
 
-let hours = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm"];
-
-function CookieStand(name, minCustomersPerHour, maxCustomersPerHour, averageCookies) {
+function City(name, minCust, maxCust, avgCookie){
   this.name = name;
-  this.minCustomersPerHour = minCustomersPerHour
-  this.maxCustomersPerHour = maxCustomersPerHour
-  this.averageCookies = averageCookies
-  this.estimatedSales = [estimatedSales];
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCookie = avgCookie;
+  this.randomCust = [];
+  // total cookies for hour
+  this.hourCookies = [];
+  this.completeCookies = [];
+  // total cookies for city
+  this.totalCookies = [];
+  this.totalSales = 0;
 }
 
-// This is where I'm stuck...I think a Protype method of the CookieStand constructor should go next but I don't understand his piece.
+let seattle = new City('Seattle',23, 65, 6.3);
+let tokyo = new City('Tokyo', 3, 24, 1.2);
+let dubai = new City('Dubai', 11, 38, 3.7);
+let paris = new City('Paris', 20, 38, 2.3);
+let lima = new City('Lima', 2, 16, 4.6);
 
-//Sample code copied from demo example - need to adjust for CookieStand, putting it here to help me try and figure it out.
-let body = document.getElementById("locations");
-   let locationRow = document.createElement('tr');
-   body.appendChild(locationRow);
+City.prototype.cookieTime = function(){
+  for(let i = 0; i<14;i++){
+    let randCust = Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
+    this.randomCust.push(randCust);
+    let estimate = (Math.floor(randCust * this.avgCookie));
+    this.hourCookies.push(estimate);
+    this.totalSales += estimate;
+    totals[i] += estimate;
+  }
 
-   let nameCell = document.createElement('td');
-   nameCell.textContent = this.name;
-   locationRow.appendChild(nameCell);
-// I don't think this is needed for CookieStand???
-   let breedCell = document.createElement('td');
-   breedCell.textContent = this.breed;
-   dogRow.appendChild(breedCell);
+};
 
-   for( let i = 0; i < this.isGoodWith.length; i++ ) {
-    let cell = document.createElement("td");
-    cell.textContent = this.isGoodWith[i].toString();
-    dogRow.appendChild(cell);
-   }
+City.prototype.render = function(){
+  let body = document.getElementById("tableBody");
+  let cityRow = document.createElement('tr');
+  body.appendChild(cityRow);
 
-// Again copied from demo code example to help me work through this but I'm stuck...
 
-let seattle = new CookieStand();
-let tokyo = new CookieStand();
+  let cityName = document.createElement('td');
+  cityName.textContent = this.name;
+  cityRow.appendChild(cityName);
 
-let state = {
-  locations: [seattle, tokyo]
-}
 
+  for(let i = 0; i < this.hourCookies.length; i ++){
+    let cell = document.createElement('td');
+    cell.textContent = (this.hourCookies[i]);
+    cityRow.appendChild(cell)
+  }
+  let cityTotal = document.createElement('td');
+  cityTotal.textContent = this.totalSales;
+  cityRow.appendChild(cityTotal);
+
+};
+
+let renderTotals = function(){
+
+  let tableFooter = document.getElementById('tableFoot');
+  let bottomTotal = document.createElement('tr');
+  bottomTotal.textContent = 'Totals';
+  tableFooter.appendChild(bottomTotal);
+
+
+  for(let i = 0; i < totals.length; i++){
+    let footCell;
+    footCell = document.createElement('td');
+    footCell.textContent = totals[i];
+    bottomTotal.appendChild(footCell);
+  }
+
+  let dailyTotal = (seattle.totalSales + tokyo.totalSales + dubai.totalSales + paris.totalSales + lima.totalSales);
+
+  let totalAll = document.createElement("td");
+  totalAll.textContent = dailyTotal;
+  bottomTotal.appendChild(totalAll);
+};
+
+seattle.cookieTime();
+tokyo.cookieTime();
+dubai.cookieTime();
+paris.cookieTime();
+lima.cookieTime();
 seattle.render();
 tokyo.render();
+dubai.render();
+paris.render();
+lima.render();
+renderTotals();
+
+
+console.log(totals);
+
 
 // Original object literals and code to add estimates to list
 
